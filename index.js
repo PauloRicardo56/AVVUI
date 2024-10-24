b65f6e95edd8cb947c61601858217354ee859f7c
Paulo Vieira <praraujo.vieira@gmail.com>
2024-10-24 17:01:36 -0300

    mend

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-export { default as AVVButton } from "./src/AVVButton";
-export { default as AVVGameTableCell } from "./src/AVVGameTableCell";
+// export { default as AVVButton } from "./src/AVVButton";
+// export { default as AVVGameTableCell } from "./src/AVVGameTableCell";
 export { default as AVVScore } from "./src/AVVScore";
-export { default as SingleScore } from "./src/SingleScore"
-export { default as AVVTable } from "./src/AVVTable";
-export { default as AVVTableCell } from "./src/AVVTableCell";
-export { default as AVVText } from "./src/AVVText";
-export { default as AVVTextField } from "./src/AVVTextField";
-export { default as AVVTitleTableCell } from "./src/AVVTitleTableCell";
+// export { default as SingleScore } from "./src/SingleScore"
+// export { default as AVVTable } from "./src/AVVTable";
+// export { default as AVVTableCell } from "./src/AVVTableCell";
+// export { default as AVVText } from "./src/AVVText";
+// export { default as AVVTextField } from "./src/AVVTextField";
+// export { default as AVVTitleTableCell } from "./src/AVVTitleTableCell";
 
 export const test = () => {
     return (
