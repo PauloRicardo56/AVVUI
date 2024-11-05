8ba26a1849c931fbf571044e840428649223bf73
Paulo Vieira <praraujo.vieira@gmail.com>
2024-11-05 19:49:35 -0300

    mend

--- src/AVVFirebase.js
+++ src/AVVFirebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { deleteDoc, doc, getDoc, getFirestore, onSnapshot, setDoc } from "firebase/firestore"
 import * as Notifications from 'expo-notifications'
-import * as Permissions from 'expo-permissions'
 
 let firebaseApp
 let db
@@ -60,16 +59,35 @@ async function deleteFirebaseDoc(collectionName="", docName="") {
 
 // Notifications
 const requestPermission = async () => {
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS)
-    if (status !== 'granted') {
-        alert('Permission for notifications was denied');
+    const { status: existingStatus } = await Notifications.getPermissionsAsync()
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
     }
 }
 
 async function getFCMToken() {
-    const token = await messaging().getToken()
-    console.log('FCM Token:', token)
-    // Send this token to your backend to register the device for notifications
+    let token
+    try {
+        const projectId = Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+        if (!projectId) {
+            throw new Error('Project ID not found');
+        }
+        token = (
+            await Notifications.getExpoPushTokenAsync({
+                projectId,
+            })
+        ).data;
+        console.log(token);
+    } catch (e) {
+        token = `${e}`;
+    }
+    return token
 }
 
 export { getFirestoreDoc }
