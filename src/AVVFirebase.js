e2a680d93eab1637e7b9e717168bfee655542564
Paulo Vieira <praraujo.vieira@gmail.com>
2024-11-05 15:07:18 -0300

    1.6.6

--- src/AVVFirebase.js
+++ src/AVVFirebase.js
@@ -1,23 +1,43 @@
 import { initializeApp } from 'firebase/app';
 import { deleteDoc, doc, getDoc, getFirestore, onSnapshot, setDoc } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyC4wsF4qxwp04pUTS29dUidxXnsaToHqiY",
-    authDomain: "aovivasco-438802.firebaseapp.com",
-    projectId: "aovivasco-438802",
-    storageBucket: "aovivasco-438802.appspot.com",
-    messagingSenderId: "236345160786",
-    appId: "1:236345160786:web:068b973796f3281dc29b8b"
-};
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
+let firebaseApp
+let db
+
+// Initialize Firebase app and Firestore only once
+export function initializeFirebase(firebaseConfig) {
+  if (!firebaseApp) {
+    firebaseApp = initializeApp(firebaseConfig)
+    db = getFirestore(firebaseApp)
+  }
+}
+
+function getFirestoreDB() {
+  if (!db) {
+    throw new Error("Firebase has not been initialized. Call initializeFirebase(firebaseConfig) first.")
+  }
+  return db
+}
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC4wsF4qxwp04pUTS29dUidxXnsaToHqiY",
+//     authDomain: "aovivasco-438802.firebaseapp.com",
+//     projectId: "aovivasco-438802",
+//     storageBucket: "aovivasco-438802.appspot.com",
+//     messagingSenderId: "236345160786",
+//     appId: "1:236345160786:web:068b973796f3281dc29b8b"
+// };
+// const app = initializeApp(firebaseConfig)
+// const db = getFirestore(app)
 
 // GET
 async function getFirestoreDoc(collection, document) {
+    const db = getFirestoreDB()
     return await getDoc(doc(db, collection, document))
 }
 
 function eventListener(collection, document, completion) {
+    const db = getFirestoreDB()
     const unsub = onSnapshot(doc(db, collection, document), (doc) => {
       completion(doc.data())
     })
@@ -37,11 +57,13 @@ async function insertIndexZeroFirebaseArray(collectionName="", docName="", field
 }
 
 async function pushFirebaseDoc(collectionName="", docName="", data) {
+    const db = getFirestoreDB()
     await setDoc(doc(db, collectionName, docName), data);
 }
 
 // DELETE
 async function deleteFirebaseDoc(collectionName="", docName="") {
+    const db = getFirestoreDB()
     await deleteDoc(doc(db, collectionName, docName));
 }
 
